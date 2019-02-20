0757145eea8511f1714951dd9c3d570d094ef6ef
Glenn Hinks <ghinks@yahoo.com>
2019-02-20 05:42:03 -0500

    remove unused or commented out code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 console.log('hello-world');
-const docHeader = document.createElement('h1');
-docHeader.innerText = 'HEADER';
-document.body.appendChild(docHeader);
 
 const element = <h1>The JSX element</h1>;
 
-
 ReactDOM.render(
-  // React.createElement('h1', null, 'Hello World Component'),
   element,
   document.getElementById('root')
 );
